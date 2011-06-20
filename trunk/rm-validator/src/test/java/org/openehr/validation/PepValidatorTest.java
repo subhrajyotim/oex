fefd3144e1a261ee87e1229ef66d33e856d218e5
inael.rodrigues <inael.rodrigues@b6674312-e529-d40e-6577-6a6397fe8104>
2011-06-20 12:33:46 +0000

    Teste do validador do person sem Items

--- trunk/rm-validator/src/test/java/org/openehr/validation/PepValidatorTest.java
+++ trunk/rm-validator/src/test/java/org/openehr/validation/PepValidatorTest.java
@@ -1,16 +1,21 @@
 package org.openehr.validation;
 
+import java.util.Collection;
+import java.util.Iterator;
 import static org.junit.Assert.*;
 import br.ufg.inf.fs.pep.archetypes.ArchetypeRepository;
 import br.ufg.inf.fs.pep.archetypes.PepArchetypeRepository;
 import java.util.List;
+import java.util.Set;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.openehr.am.archetype.Archetype;
+import org.openehr.rm.datastructure.itemstructure.ItemStructure;
+import org.openehr.rm.datastructure.itemstructure.ItemTree;
 import org.openehr.rm.datastructure.itemstructure.representation.Element;
 import org.openehr.rm.datatypes.quantity.datetime.DvDateTime;
 import org.openehr.rm.datatypes.text.DvText;
+import org.openehr.rm.demographic.Party;
 import org.openehr.rm.demographic.Person;
 
 /**
@@ -23,13 +28,13 @@ public class PepValidatorTest extends PepBaseTest {
     DataValidator validator = null;
 
     @Before
-    public void setUp(){
+    public void setUp() {
         this.repository = new PepArchetypeRepository();
         this.validator = new DataValidatorImpl();
     }
 
     @Test
-    public void testDvDateTime() throws Exception{
+    public void testDvDateTime() throws Exception {
         DvDateTime dateTime = new DvDateTime();
         Element element = new Element("at0000", "Período da última menstruação normal", dateTime);
         String archetypeId = "openEHR-EHR-ELEMENT.last_normal_menstrual_period.v1";
@@ -37,13 +42,13 @@ public class PepValidatorTest extends PepBaseTest {
         Archetype archetype = this.repository.getArchetype(archetypeId);
 
         List<ValidationError> errors = null;
-        
+
         errors = this.validator.validate(element, archetype);
         assertTrue("The list must not contain error", errors.isEmpty());
     }
 
     @Test
-    public void testDvText() throws Exception{
+    public void testDvText() throws Exception {
         DvText text = new DvText("Universidade Federal de Goiás");
         Element element = new Element("at0001", "Arquétipo de teste DVTEXT", text);
         String archetypeId = "openEHR-EHR-ELEMENT.test_dvtext.v1";
@@ -55,9 +60,8 @@ public class PepValidatorTest extends PepBaseTest {
         assertTrue("The list must not contain error", errors.isEmpty());
     }
 
-    @Ignore
     @Test
-    public void testPerson() throws Exception{
+    public void testPerson() throws Exception {
         Person person = this.getPerson();
         String archetypeId = "openEHR-DEMOGRAPHIC-PERSON.person.v1";
         Archetype archetype = this.repository.getArchetype(archetypeId);
@@ -66,12 +70,48 @@ public class PepValidatorTest extends PepBaseTest {
         errors = this.validator.validate(person, archetype);
 
         for (ValidationError validationError : errors) {
-            System.out.println( validationError.getErrorType() +
-                    " \nRuntime path : " + validationError.getRuntimePath() +
-                    "\nArchetype path : " + validationError.getArchetypePath() );
+            System.out.println(validationError.getErrorType()
+                    + " \nRuntime path : " + validationError.getRuntimePath()
+                    + "\nArchetype path : " + validationError.getArchetypePath());
             System.out.println("");
         }
         assertTrue("The list must be null", errors.isEmpty());
     }
-    
+
+    @Test
+    public void testPersonWithoutItems() throws Exception {
+        Person person = this.getPerson();
+        ((ItemTree) person.getDetails()).setItems(null);
+        assertTrue(erroEsperadoFoiEncontrado(ErrorType.ATTRIBUTE_MISSING, person));
+    }
+
+
+    /**
+     * Responsável por verificar se o validador está realmente pegando o erro.
+     * Para isso é preciso de um Person  com algum inconsistência em relação às
+     * restrições que o arquétipo impoem e o ErrorType equivalente à inconsistência.
+     * @param errortype Tipo de erro que deverá ser compativel com inconsistência
+     * injetada no person.
+     * @param person Objeto com alguma inconsistência para ser testado.
+     * @throws Exception
+     */
+    private boolean erroEsperadoFoiEncontrado(ErrorType errortype, Person person) throws Exception {
+        boolean erroEsperadoFoiCapturado = false;
+        String archetypeId = "openEHR-DEMOGRAPHIC-PERSON.person.v1";
+        Archetype archetype = this.repository.getArchetype(archetypeId);
+
+        List<ValidationError> errors = null;
+        errors = this.validator.validate(person, archetype);
+
+        for (ValidationError validationError : errors) {
+            if (validationError.getErrorType() == errortype) {
+                erroEsperadoFoiCapturado = true;
+            }
+        }
+        return erroEsperadoFoiCapturado;
+    }
 }
+
+
+
+
