daecefe88917f640da2d8e51c0fa49342dd3b42a
inael.rodrigues <inael.rodrigues@b6674312-e529-d40e-6577-6a6397fe8104>
2011-06-20 20:44:05 +0000

    novo teste: person com details null

--- rm-validator/src/test/java/org/openehr/validation/PepValidatorTest.java
+++ rm-validator/src/test/java/org/openehr/validation/PepValidatorTest.java
@@ -1,23 +1,27 @@
 package org.openehr.validation;
 
-import java.util.Collection;
-import java.util.Iterator;
 import static org.junit.Assert.*;
 import br.ufg.inf.fs.pep.archetypes.ArchetypeRepository;
 import br.ufg.inf.fs.pep.archetypes.PepArchetypeRepository;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.openehr.am.archetype.Archetype;
-import org.openehr.rm.datastructure.itemstructure.ItemStructure;
+import org.openehr.rm.common.archetyped.Archetyped;
 import org.openehr.rm.datastructure.itemstructure.ItemTree;
+import org.openehr.rm.datastructure.itemstructure.representation.Cluster;
 import org.openehr.rm.datastructure.itemstructure.representation.Element;
 import org.openehr.rm.datatypes.quantity.datetime.DvDateTime;
 import org.openehr.rm.datatypes.text.DvText;
-import org.openehr.rm.demographic.Party;
+import org.openehr.rm.demographic.Address;
+import org.openehr.rm.demographic.Contact;
+import org.openehr.rm.demographic.PartyIdentity;
 import org.openehr.rm.demographic.Person;
+import org.openehr.rm.support.identification.ArchetypeID;
+import org.openehr.rm.support.identification.ObjectVersionID;
 
 /**
  *
@@ -79,6 +83,25 @@ public class PepValidatorTest extends PepBaseTest {
         assertTrue("The list must be null", errors.isEmpty());
     }
 
+    @Test
+    public void testPersonWithoutDetails() throws Exception {
+        Person person = null;
+        // PartyIdentity
+        PartyIdentity partyIdentity = getPersonName(person);
+        Set<PartyIdentity> identities = new HashSet<PartyIdentity>();
+        identities.add(partyIdentity);
+        Address address = getContact();
+        List listAddresses = new ArrayList();
+        listAddresses.add(address);
+        Contact contact = new Contact(null, "at0003", new DvText("Contact"), null, null, null, person, null, listAddresses);
+        Set<Contact> contacts = new HashSet();
+        contacts.add(contact);
+        ObjectVersionID objectVersionID = new ObjectVersionID("939cec48-d629-4a3f-89f1-28c573387680::"
+                + "10aec661-5458-4ff6-8e63-c2265537196d::1");
+        person = new Person(objectVersionID, "openEHR-DEMOGRAPHIC-PERSON.person.v1.adl", new DvText("person demographic data"), new Archetyped(new ArchetypeID("openEHR-DEMOGRAPHIC-PERSON.person.v1"), "1.0.0.0"), null, null, identities, contacts, null, null, null, null, null);
+
+        assertTrue(erroEsperadoFoiEncontrado(ErrorType.ATTRIBUTE_MISSING, person));
+    }
 
     @Test
     public void testPersonWithoutItems() throws Exception {
@@ -86,7 +109,6 @@ public class PepValidatorTest extends PepBaseTest {
         ((ItemTree) person.getDetails()).setItems(null);
         assertTrue(erroEsperadoFoiEncontrado(ErrorType.ATTRIBUTE_MISSING, person));
     }
-
 
     /**
      * Responsável por verificar se o validador está realmente pegando o erro.
@@ -108,12 +130,9 @@ public class PepValidatorTest extends PepBaseTest {
         for (ValidationError validationError : errors) {
             if (validationError.getErrorType() == errortype) {
                 erroEsperadoFoiCapturado = true;
+
             }
         }
         return erroEsperadoFoiCapturado;
     }
 }
-
-
-
-
